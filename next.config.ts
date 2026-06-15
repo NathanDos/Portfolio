4bcf859c18b97742e2f2718cf2d770b78f761e62
Nathan Dos Santos <ndossantos1@yahoo.ca>
2026-06-15 19:48:17 -0400

    static export

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
+  output: 'export',  // <=== enables static exports
+  images: {
+    unoptimized: true, // Mandatory for static HTML exports
+  },
   reactStrictMode: true,
   allowedDevOrigins: ['192.168.86.22'],
   basePath: '/Portfolio',
