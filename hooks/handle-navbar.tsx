65a745c55a73d9e1b7d444343e442143cb6b3142
Nathan Dos Santos <ndossantos1@yahoo.ca>
2026-07-02 16:37:30 -0400

    Handle-Navbar chrome functionality

--- hooks/handle-navbar.tsx
+++ hooks/handle-navbar.tsx
@@ -3,24 +3,31 @@ import { useEffect, useRef, useState } from 'react';
 
 // Handle the navbar visibility based on scroll and mouse position
 export function handleNavbar(navSize: number = 50) {
-    const [isVisible, setIsVisible] = useState(true);
-    const isAtTopRef = useRef(true);
+    const [isVisible, setIsVisible] : [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(true);
+    const isAtTopRef : React.RefObject<boolean> = useRef(true);
 
     useEffect(() => {
         const handleMouseMove = (event: MouseEvent) => {
             // If the mouse is near the top of the screen, show the navbar
-            if (event.clientY < navSize) {
+            if(event.clientY < navSize) {
                 setIsVisible(true);
-            } else if (!isAtTopRef.current) {
+            } else if(isAtTopRef.current) {
+                setIsVisible(true);
+            }
+            else {
                 setIsVisible(false);
             }
         };
+
         // Handle scroll event to hide/show navbar based on scroll position
         const handleScrollEvent = () => {
-            isAtTopRef.current = window.scrollY === 0;
-            setIsVisible(isAtTopRef.current);
+            let atTop : boolean = window.scrollY === 0;
+            isAtTopRef.current = atTop;
+            if(atTop){
+                setIsVisible(true);
+            }
         };
-
+        
         window.addEventListener('mousemove', handleMouseMove);
         window.addEventListener('scroll', handleScrollEvent);
 
@@ -29,5 +36,8 @@ export function handleNavbar(navSize: number = 50) {
             window.removeEventListener('scroll', handleScrollEvent);
         };
     }, [navSize]);
+        useEffect(() => {
+        console.log("isVisible:", isVisible);
+    }, [isVisible]);
     return { isVisible };
 }
