4218e758564653aec76c09f13157977c3d998aac
Nathan Dos Santos <ndossantos1@yahoo.ca>
2026-07-06 22:51:23 -0400

    Simple Footer space created

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,26 @@
-"use client";
+'use client';
 //Custom Components
-import Navbar from "@/components/navbar";
-import ScrollingWorkInformation from "@/components/scrolling-work-information";
-import CarouselBuilder from "@/components/carousel-builder";
-import ExpandableCardGrid from "@/components/expandable-card-grid";
-import { TracingBeam } from "@/components/ui/tracing-beam";
+import Navbar from '@/components/navbar';
+import ScrollingWorkInformation from '@/components/scrolling-work-information';
+import CarouselBuilder from '@/components/carousel-builder';
+import ExpandableCardGrid from '@/components/expandable-card-grid';
+import { TracingBeam } from '@/components/ui/tracing-beam';
+import Footer from '@/components/footer';
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <TracingBeam>
-        <section id="work-experience" />
+        <section id='work-experience' />
         <ScrollingWorkInformation />
-        <section id="strengths" />
+        <section id='strengths' />
         <CarouselBuilder />
-        <section id="projects" />
+        <section id='projects' />
         <ExpandableCardGrid />
       </TracingBeam>
-      <section id="footer-section" />
+      <Footer />
+      <section id='footer-section' />
     </>
   );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className='w-full h-32 bg-gray-800 text-white flex items-center justify-center'>
+      <h1 className='text-4xl font-bold'>Contact Info:</h1>
+    </div>
+  );
+}
