efee29873ccf32395fe008e9561c02f7024d5626
Nathan Dos Santos <ndossantos1@yahoo.ca>
2026-07-13 17:39:36 -0400

    prettier changes & css canonical classes

--- components/expandable-card-content.tsx
+++ components/expandable-card-content.tsx
@@ -1,19 +1,27 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 //custom hooks
-import { useOverflowMask } from "../hooks/use-overflow-mask";
+import { useOverflowMask } from '../hooks/use-overflow-mask';
 //custom components
-import { FadeOverlay } from "./styling/fade-overlay";
+import { FadeOverlay } from './styling/fade-overlay';
 //custom data
-import { cards } from "../data/portfolio-cards";
+import { cards } from '../data/portfolio-cards';
 
-export function ExpandedCardContent({ active, id, animatedGridRef }: { active: (typeof cards)[number]; id: string; animatedGridRef: React.RefObject<HTMLDivElement> }) {
+export function ExpandedCardContent({
+  active,
+  id,
+  animatedGridRef,
+}: {
+  active: (typeof cards)[number];
+  id: string;
+  animatedGridRef: React.RefObject<HTMLDivElement>;
+}) {
   const { scrollRef, showMask, handleScroll } = useOverflowMask(active);
 
   return (
     <motion.div
       layoutId={`card-${active.title}-${id}`}
       ref={animatedGridRef}
-      className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+      className='w-full max-w-125 h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden'
     >
       <motion.div layoutId={`image-${active.title}-${id}`}>
         <img
@@ -21,21 +29,21 @@ export function ExpandedCardContent({ active, id, animatedGridRef }: { active: (
           height={200}
           src={active.src}
           alt={active.title}
-          className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+          className='w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top'
         />
       </motion.div>
       <div>
-        <div className="flex justify-between items-start p-4">
+        <div className='flex justify-between items-start p-4'>
           <div>
             <motion.h3
               layoutId={`title-${active.title}-${id}`}
-              className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
+              className='font-medium text-neutral-700 dark:text-neutral-200 text-base'
             >
               {active.title}
             </motion.h3>
             <motion.p
               layoutId={`description-${active.description}-${id}`}
-              className="text-neutral-600 dark:text-neutral-400 text-base"
+              className='text-neutral-600 dark:text-neutral-400 text-base'
             >
               {active.description}
             </motion.p>
@@ -46,13 +54,13 @@ export function ExpandedCardContent({ active, id, animatedGridRef }: { active: (
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             href={active.ctaLink}
-            target="_blank"
-            className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
+            target='_blank'
+            className='px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white'
           >
             {active.ctaText}
           </motion.a>
         </div>
-        <div className="pt-4 relative px-4">
+        <div className='pt-4 relative px-4'>
           <motion.div
             layout
             layoutDependency={active.title}
@@ -61,10 +69,12 @@ export function ExpandedCardContent({ active, id, animatedGridRef }: { active: (
             exit={{ opacity: 0 }}
             ref={scrollRef}
             onScroll={handleScroll}
-            className="text-neutral-600 text-xs md:text-sm lg:text-base max-h-100 pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+            className='text-neutral-600 text-xs md:text-sm lg:text-base max-h-100 pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 scrollbar-none [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]'
           >
-            <FadeOverlay show={showMask} size="lg" />
-            {typeof active.content === "function" ? active.content() : active.content}
+            <FadeOverlay show={showMask} size='lg' />
+            {typeof active.content === 'function'
+              ? active.content()
+              : active.content}
           </motion.div>
         </div>
       </div>
